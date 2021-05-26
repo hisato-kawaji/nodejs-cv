9b5ed472960aedc27c908599dffa36e9a1753e21
hisato-kawaji <hkawaji@moff.mobi>
2021-05-26 16:28:12 +0900

    fixed sample code

---
+++ opencv/sample.js
@@ -0,0 +1,26 @@
+const Jimp = require('jimp');
+async function onRuntimeInitialized(){
+  var jimpSrc = await Jimp.read('../lena.png');
+
+  var src = cv.matFromImageData(jimpSrc.bitmap);
+
+  let dst = new cv.Mat();
+  let M = cv.Mat.ones(5, 5, cv.CV_8U);
+  let anchor = new cv.Point(-1, -1);
+  cv.dilate(src, dst, M, anchor, 1, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
+
+  new Jimp({
+    width: dst.cols,
+    height: dst.rows,
+    data: Buffer.from(dst.data)
+  })
+  .write('output.png');
+  src.delete();
+  dst.delete();
+}
+
+Module = {
+  onRuntimeInitialized
+};
+
+cv = require('./opencv.js');
